a9bbaa7cd0a7fd5c38f4740a65d4347d4d24aab5
subo <82173071+subo-9439@users.noreply.github.com>
2022-06-18 18:54:47 +0900

    [Gold V] Title: 공유기 설치, Time: 420 ms, Memory: 40960 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -4,92 +4,75 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
     static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
     static int N, C;
-    static int[] home;
-
-    static void input() {
-        N = scan.nextInt();
-        C = scan.nextInt();
-        home = new int[N+1];
-
-        for(int i = 1; i <= N; i++) {
-            home[i] = scan.nextInt();
-        }
+    static int[] arr;
+    static boolean[] checked;
+    static void input(){
+        N = scan.nextInt(); C = scan.nextInt();
+        arr = new int[N];
+        checked = new boolean[N];
+        for (int i = 0; i < arr.length; i++) arr[i] = scan.nextInt();
     }
-
-    static boolean determination(int D){
-        //D 만큼의 거리 차이를 둔다면 C 개만큼의 공유기를 설치할 수 있는가?
-
-        //제일 왼쪽 집부터 가능한 많이 설치해야한다.
-        //D만큼의 거리를 두면서 최대한 설치한 개수와 c를 비교하자.
-
-        //첫번째 집에 하나를 설치하니까 cnt = 1;
-        int cnt = 1, last = home[1];
-
-        //2번집부터 N번집까지 확인을한다.
-        for (int i = 2; i <= N; i++) {
-            //i번집과 그전 집의 차이가 D보다 작으면 설치를 할 수 없다.
-            if(home[i] - last < D) continue;
-            //설치했으므로 last를 갱신시켜주고 cnt++해준다.
-            last = home[i];
-            cnt++;
-        }
-        //C개 이상 설치했을 때만 돌려준다.
-        return cnt >= C;
-    }
-    static void pro() {
-        //determination을 빠르게 하기 위해서 정렬해야한다.
-        Arrays.sort(home,1, N+1);
-
-        int L = 1, R = 1000000000, ans = 0;
-        //[L...R] 범위 안에 정답이 존재한다.
-        //이분 탐색과 determination 문제를 이용해서 answer를 빠르게 구하자
-
-        while(L <= R) {
-            int mid = (L+R)/2;
-            if(determination(mid)){
-                ans = mid;
-                L = mid +1;
-            } else {
-                R = mid -1;
+    static void pro(){
+        Arrays.sort(arr);
+        int ans =0;
+//        int L = 0, R = 1000000000;   //최대 나올 수 있는 거리차이는 arr[0]에서 arr[N-1]까지
+        int L = 1,R = arr[N-1];   //최대 나올 수 있는 거리차이는 arr[0]에서 arr[N-1]까지
+        while (L <= R){
+            int dist = (L+R)/2;
+            if(canRouter(dist)){//공유기를 C개 이상 설치할 수 있으니까 한번 거리를 늘리자 -> 집과 집사이의 거리를 늘리면 설치할 수 있는 공유기가 줄어든다.
+                L = dist + 1;
+                ans = dist;
+//                System.out.println("현재값은 : " + ans);
+            }else {//공유기를 C개도 설치 못하니까 설치간격을 좁혀야한다.
+                R = dist - 1;
             }
         }
         System.out.println(ans);
+    }
+    static boolean canRouter(int dist){     //dist 변수는 집간격이 될 수 있는 최소의 값
+        int pos = arr[0]; //현재 공유기 설치위치
+        int cnt = 1;      //설치한 공유기 대수
+
+        for (int i = 1; i < N; i++){
+            if(arr[i] - pos >= dist) {
+                cnt++;    //(공유기가 설치된)집과 집사이의 거리가 dist 보다 크거나 같으면 공유기를 설치하자!
+                pos = arr[i]; // 공유기 위치 갱신
+            }
+
+        }
+        return cnt >= C;            //공유기 개수가 C보다 많으면 true
     }
     public static void main(String[] args) {
         input();
         pro();
     }
-    static class FastReader {
+    static class FastReader{
         BufferedReader br;
         StringTokenizer st;
-        public FastReader() {
+
+        FastReader(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
         String next(){
-            while(st == null || !st.hasMoreElements()){
-                try{
-                    st = new StringTokenizer(br.readLine());
-                }catch(IOException e){
+            while (st == null || !st.hasMoreTokens()){
+                try {
+                     st = new StringTokenizer(br.readLine());
+                }catch (IOException e){
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
+
         int nextInt(){
             return Integer.parseInt(next());
+        }
+
+        long nextLong(){
+            return Long.parseLong(next());
         }
     }
 }
