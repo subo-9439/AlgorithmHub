36ef80039c0e75f6d2b4dac469b9e26ffa3621a9
subo <82173071+subo-9439@users.noreply.github.com>
2022-06-24 20:59:43 +0900

    [Silver II] Title: 약수의 합 2, Time: 136 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    public static void main(String[] args) {
+        int N = scan.nextInt();
+        long sum = 0;
+        for (int i = 1; i <= N; i++){
+            sum += (long) i * (N/i);
+        }
+        System.out.println(sum);
+    }
+
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next(){
+            while (st == null ||!st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+}
