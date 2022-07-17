648082a1bbaeb79713d425e445a4a6469f9e07b1
subo <82173071+subo-9439@users.noreply.github.com>
2022-07-17 16:40:10 +0900

    [Silver I] Title: 회의실 배정, Time: 556 ms, Memory: 47196 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,37 +1,67 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int timeTable[][] = new int[N][2];
-
-        for (int i = 0; i < N; i++) {
-            timeTable[i][0] = sc.nextInt();
-            timeTable[i][1] = sc.nextInt();
+    static FastReader scan = new FastReader();
+    static int N;
+    static int[][] info;
+    static void input(){
+        N = scan.nextInt();
+        info = new int[N][2];
+        for (int i = 0; i < N; i++){
+            int start = scan.nextInt();
+            int end = scan.nextInt();
+            info[i][0] = start;
+            info[i][1] = end;
         }
-        Arrays.sort(timeTable, (a, b) -> {
-            if(a[1] == b[1]) return a[0] - b[0];
-            return a[1] - b[1];
+    }
+    static void pro() {
+        //회의실 배정을 하기 위해선
+        //회의가 가장 빨리 끝나는 순으로 배열을 해야한다.
+        //끝나는 시점이 같다면 가장 먼저 시작하는 순으로
+        // start , end -> 둘다 오름차순
+        Arrays.sort(info, (o1, o2) -> {
+            if (o1[1] == o2[1]) return o1[0] - o2[0];
+            return o1[1] - o2[1];
         });
-/*
-		System.out.println("종료 시간에 맞춰 정렬");
-		for (int i = 0; i < N; i++) {
-			System.out.println(timeTable[i][0] + " " + timeTable[i][1]);
-		}
-*/
-        int ans = 0;
-        int time = 0;
-
-        for (int i = 0; i < N; i++) {
-            // 다음회의시작시각 >= 이전회의종료시각
-            if (timeTable[i][0] >= time) {
-                time = timeTable[i][1];
-                ans++;
+        int cur = 0;
+        int cnt = 0;
+        for (int i = 0 ; i < info.length; i++){
+            if (info[i][0] >= cur){
+                cnt++;
+                cur = info[i][1] ;
             }
         }
-        System.out.println(ans);
+        System.out.println(cnt);
+    }
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while (st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
     }
 }
