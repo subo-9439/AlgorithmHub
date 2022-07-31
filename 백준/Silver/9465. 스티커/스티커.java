86fe0936c15d539ef85139e761509b2cd0b0ce90
subo <82173071+subo-9439@users.noreply.github.com>
2022-08-01 08:51:25 +0900

    [Silver I] Title: 스티커, Time: 936 ms, Memory: 119348 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -6,26 +6,28 @@ import java.util.StringTokenizer;
 public class Main {
     static FastReader scan = new FastReader();
     static int N;
-    static int[][] dp;
+    static int[][] arr,dp;
     static boolean[] visited;
     public static void main(String[] args) {
         int T = scan.nextInt();
         for (int t = 0; t < T; t++){
             N = scan.nextInt();
-            dp = new int[N+1][3];
-            visited = new boolean[N+1];
-            for (int i = 1; i <= N; i++)dp[i][1] = scan.nextInt();
-            for (int i = 1; i <= N; i++)dp[i][2] = scan.nextInt();
-            pro();
+            //시작
+            arr = new int[2][N + 1];
+            dp = new int[2][N + 1];
+            for (int j = 0; j < 2; j++) { //초기화
+                for (int k = 1; k <= N; k++) {
+                    arr[j][k] = scan.nextInt();
+                }
+            }
+            dp[0][1] = arr[0][1];
+            dp[1][1] = arr[1][1];
+            for (int a = 2; a <= N; a++) {
+                dp[0][a] = Math.max(dp[1][a - 1], dp[1][a - 2]) + arr[0][a];
+                dp[1][a] = Math.max(dp[0][a - 1], dp[0][a - 2]) + arr[1][a];
+            }
+            System.out.println(Math.max(dp[0][N], dp[1][N]));
         }
-    }
-    static void pro(){
-        for (int i = 1; i <= N; i++){
-            dp[i][0] += Math.max(dp[i-1][1],dp[i-1][2]);
-            dp[i][1] += Math.max(dp[i-1][0],dp[i-1][2]);
-            dp[i][2] += Math.max(dp[i-1][0],dp[i-1][1]);
-        }
-        System.out.println(Math.max(Math.max(dp[N][0], dp[N][1]),dp[N][2]));
     }
     static class FastReader{
         BufferedReader br;
