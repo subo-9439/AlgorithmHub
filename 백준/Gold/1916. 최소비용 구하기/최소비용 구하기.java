5df79509ea8b427332910dd7f198e14139c15413
subo <82173071+subo-9439@users.noreply.github.com>
2022-09-12 14:24:30 +0900

    [Gold V] Title: 최소비용 구하기, Time: 556 ms, Memory: 53424 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,77 +1,70 @@
+import org.w3c.dom.Node;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
     static FastReader scan = new FastReader();
-    static int N,M,start,end;
-    static ArrayList<State>[] nodes;
-    static boolean[] visited;
+    static int N,M;
+    static ArrayList<Node>[] edges;
+    static int start,end;
     static int[] dp;
 
     static void input(){
-        N = scan.nextInt();
-        M = scan.nextInt();
-        nodes = new ArrayList[N+1];
+        N = scan.nextInt(); M = scan.nextInt();
+        edges = new ArrayList[N+1];
         dp = new int[N+1];
+        for (int i = 1; i <= N; i++) edges[i] = new ArrayList<>();
 
-        for (int i = 1; i <= N; i++) {
-            nodes[i] = new ArrayList<>();
-        }
-        for (int i = 1; i <= M; i++) {
-            int x = scan.nextInt(), y = scan.nextInt(), w = scan.nextInt();
-            nodes[x].add(new State(y,w));
-        }
-        start = scan.nextInt();
-        end = scan.nextInt();
+        for (int i = 1; i <= M; i++) edges[scan.nextInt()].add(new Node(scan.nextInt(), scan.nextInt()));
+
+        start = scan.nextInt(); end = scan.nextInt();
     }
     static void pro(){
-        //시작점과 끝점이 정해져 있다.
-        //시작점부터 각 노드에 대해 얼마나 적은값으로 갈 수 있는지 갱신을 해주면서 가면된다.
         Arrays.fill(dp,Integer.MAX_VALUE);
-        Queue<Dist> queue = new PriorityQueue<>((o1,o2) -> o1.value - o2.value);
-        queue.add(new Dist(start,0));
+        Queue<Info> queue = new PriorityQueue<>(((o1, o2) -> o1.dist-o2.dist));
+        queue.add(new Info(start,0));
         dp[start] = 0;
 
         while (!queue.isEmpty()){
-            Dist x = queue.poll();
-            if (x.value != dp[x.node]) continue;
+            Info cur = queue.poll();
+            if (dp[cur.idx] != cur.dist) continue;
 
-            for (State y : nodes[x.node]){
-                if (dp[y.node] > dp[x.node] + y.weight){
-                    dp[y.node] = dp[x.node] + y.weight;
-                    queue.add(new Dist(y.node,dp[y.node]));
+            for (Node next : edges[cur.idx]){
+
+                if (dp[next.idx] > cur.dist + next.weight){
+                    dp[next.idx] = cur.dist + next.weight;
+                    queue.add(new Info(next.idx, cur.dist + next.weight));
                 }
             }
         }
-
     }
 
-    static class Dist{
-        int node;
-        int value;
-        Dist(int node, int value){
-            this.node = node;
-            this.value = value;
-        }
-
-    }
-    static class State{
-        int node;
-        int weight;
-        State(int node, int weight){
-            this.node = node;
-            this.weight = weight;
-        }
-    }
     public static void main(String[] args) {
         input();
         pro();
         System.out.println(dp[end]);
     }
 
+    static class Node {
+        int idx;
+        int weight;
+        Node(int idx, int weight){
+            this.idx = idx;
+            this.weight = weight;
+        }
+
+    }
+    static class Info {
+        int idx;
+        int dist;
+        Info(int idx, int dist){
+            this.idx = idx;
+            this.dist = dist;
+        }
+    }
     static class FastReader{
         BufferedReader br;
         StringTokenizer st;
@@ -79,11 +72,10 @@ public class Main {
         FastReader(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
-
         String next(){
             while (st == null || !st.hasMoreElements()){
                 try {
-                     st = new StringTokenizer(br.readLine());
+                    st = new StringTokenizer(br.readLine());
                 }catch (IOException e){
                     e.printStackTrace();
                 }
@@ -94,7 +86,5 @@ public class Main {
         int nextInt(){
             return Integer.parseInt(next());
         }
-
-
     }
 }
