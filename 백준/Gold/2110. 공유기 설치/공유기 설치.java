d856e291b1527d480b11d66ce37db99f2c1f5cd1
subo <82173071+subo-9439@users.noreply.github.com>
2022-09-14 22:53:34 +0900

    [Gold IV] Title: 공유기 설치, Time: 320 ms, Memory: 29136 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -5,74 +5,57 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static FastReader scan = new FastReader();
-    static int N, C;
-    static int[] arr;
-    static boolean[] checked;
-    static void input(){
-        N = scan.nextInt(); C = scan.nextInt();
-        arr = new int[N];
-        checked = new boolean[N];
-        for (int i = 0; i < arr.length; i++) arr[i] = scan.nextInt();
-    }
-    static void pro(){
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
         Arrays.sort(arr);
-        int ans =0;
-//        int L = 0, R = 1000000000;   //최대 나올 수 있는 거리차이는 arr[0]에서 arr[N-1]까지
-        int L = 1,R = arr[N-1];   //최대 나올 수 있는 거리차이는 arr[0]에서 arr[N-1]까지
-        while (L <= R){
-            int dist = (L+R)/2;
-            if(canRouter(dist)){//공유기를 C개 이상 설치할 수 있으니까 한번 거리를 늘리자 -> 집과 집사이의 거리를 늘리면 설치할 수 있는 공유기가 줄어든다.
-                L = dist + 1;
-                ans = dist;
-//                System.out.println("현재값은 : " + ans);
-            }else {//공유기를 C개도 설치 못하니까 설치간격을 좁혀야한다.
-                R = dist - 1;
+
+        int l = 1; // 최소로 가질 수 있는 거리
+        int r = arr[arr.length - 1] - arr[0]; //최대로 가질 수 있는 거리
+        /*
+            위 l/r을 기준으로, 가질 수 있는 거리를 이분탐색한다. (탐색위치는 대충 x라 하자)
+            좀 더 구체적으로 말해서, 지금 하고 싶은 건, 설치가 가능하면서도 가장 큰 거리 찾기 & 불가능한건 가능한 빠르게 제끼기.
+            위의 요구사항을 감안해서 실행 계획을 짜보자.
+
+            탐색위치에서 c개만큼 설치가 가능한지,
+                가능하다면 -> l = 가능했던 위치 + 1; //가능한 위치를 하나씩 올릴거야, 불가능할 때까지 / '불가능해지는 위치 - 1' = '가능한 최대 간격')
+                불가능하다면 -> r = 불가능했던 위치; //불가능했던 위치는 더 고려할 필요가 없다 && l이 r(불가능해지는 위치)까지 올라오는 시점을 체크할 용도
+        */
+        int ans = 0;
+        while (l <= r) {     //4 5
+            int x = (r + l) / 2; //4
+
+            if (canInstall(arr, x, c)) { // 설치가 가능한지 : 가능
+                l = x + 1;
+                ans = x;
+            }
+            else { // 설치가 불가능
+                r = x-1;
             }
         }
-        System.out.println(ans);
-    }
-    static boolean canRouter(int dist){     //dist 변수는 집간격이 될 수 있는 최소의 값
-        int pos = arr[0]; //현재 공유기 설치위치
-        int cnt = 1;      //설치한 공유기 대수
 
-        for (int i = 1; i < N; i++){
-            if(arr[i] - pos >= dist) {
-                cnt++;    //(공유기가 설치된)집과 집사이의 거리가 dist 보다 크거나 같으면 공유기를 설치하자!
-                pos = arr[i]; // 공유기 위치 갱신
+        System.out.println(ans); // (불가능해지는 간격 - 1) => 가능한 최대 간격
+    }
+
+    private static boolean canInstall(int[] arr, int x, int c) {
+        int count = 1; //처음 한 개는 무조건 설치
+        int installed = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (installed + x <= arr[i]) {
+                installed = arr[i];
+                count++;
             }
-
+            if (count == c) return true;
         }
-        return cnt >= C;            //공유기 개수가 C보다 많으면 true
+        return false;
     }
-    public static void main(String[] args) {
-        input();
-        pro();
-    }
-    static class FastReader{
-        BufferedReader br;
-        StringTokenizer st;
 
-        FastReader(){
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-        String next(){
-            while (st == null || !st.hasMoreTokens()){
-                try {
-                     st = new StringTokenizer(br.readLine());
-                }catch (IOException e){
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt(){
-            return Integer.parseInt(next());
-        }
-
-        long nextLong(){
-            return Long.parseLong(next());
-        }
-    }
 }
