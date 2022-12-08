13bef3907ec43bba8bc00d1296a37a6addc63d3c
subo <82173071+subo-9439@users.noreply.github.com>
2022-12-09 00:34:08 +0900

    [level 2] Title: 모음 사전, Time: 14.75 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,23 +1,13 @@
-import java.util.ArrayList;
-import java.util.Collections;
-//못품 ㅠ
+import java.util.*;
 class Solution {
-    char[] alphabet = {'A','E','I','O','U'};
-    ArrayList<String> list = new ArrayList<>();
-
-    public int solution(String word) {
-        int answer = 0;
-        permutation(0,"");
-        Collections.sort(list);
-
-        return list.indexOf(word) +1;
+    List<String> list = new ArrayList<>();
+    void dfs(String str, int len) {
+        if(len > 5) return;
+        list.add(str);
+        for(int i = 0; i < 5; i++) dfs(str + "AEIOU".charAt(i), len + 1);
     }
-
-    public void permutation(int idx, String str) {
-        if (idx >= 5) return;
-        for (int i = 0; i < alphabet.length; i++){
-            list.add(str+alphabet[i]);
-            permutation(idx+1,str+alphabet[i]);
-        }
+    public int solution(String word) {
+        dfs("", 0);
+        return list.indexOf(word);
     }
 }
