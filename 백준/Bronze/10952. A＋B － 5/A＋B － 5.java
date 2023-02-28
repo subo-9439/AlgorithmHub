9c35001d7e9370442043b5714b4e818db46ad8a0
subo <82173071+subo-9439@users.noreply.github.com>
2023-02-28 16:32:18 +0900

    [Bronze V] Title: A+B - 5, Time: 140 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static int A,B;
+    public static void main(String[] args) {
+        while (true){
+
+            A = scan.nextInt();
+            B = scan.nextInt();
+            if (A==0 && B==0){
+                break;
+            }
+            System.out.println(A+B);
+        }
+    }
+
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+
+    }
+}
