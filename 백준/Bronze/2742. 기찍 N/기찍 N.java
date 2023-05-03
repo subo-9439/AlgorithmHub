59ff392cd3dae37b11d2b92fb4d424417b5cba48
subo <82173071+subo-9439@users.noreply.github.com>
2023-05-03 10:26:05 +0900

    [Bronze IV] Title: 기찍 N, Time: 232 ms, Memory: 17924 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    public static void main(String[] args) {
+        int n = scan.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for(int i = n; i >= 1; i--) {
+            sb.append(i).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine(){
+            String str ="";
+            try {
+                str = br.readLine();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+}
