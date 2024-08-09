022f553e1fb4cb2c02482383125a56dcc5296f12
subo <82173071+subo-9439@users.noreply.github.com>
2024-08-09 09:03:10 +0900

    [Gold IV] Title: 문자열 제거, Time: 184 ms, Memory: 17916 KB -BaekjoonHub

--- 백준/Gold/21941. 문자열 제거/문자열 제거.java
+++ 백준/Gold/21941. 문자열 제거/문자열 제거.java
@@ -1,85 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-
-//풀이실패 답지확인
-public class Main {
-    static ArrayList<ArrayList<Pair>> strings; // 각 인덱스에서 지울 수 있는 부분 문자열
-    static int[] dp;
-    static class  Pair {
-        public int first;
-        public int second;
-
-        public Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
+class Main{
+    static class pair {
+        int siz, score;
+        pair(int _siz, int _score) {
+            this.siz = _siz;
+            this.score = _score;
         }
-        
     }
-    public static void main(String[] args) {
-        FastReader scan = new FastReader();
-        String S = scan.next();
-        dp = new int[S.length()+1];
-        strings = new ArrayList<>();
-        for (int i = 0; i < S.length(); i++) {
-            strings.add(new ArrayList<>());
-        }
 
-        //부분 문자열을 하나씩 확인하며
-        int M = scan.nextInt();
-        for (int i = 0; i < M; i++) {
-            //부분 문자열 입력받기
-            String subStr = scan.next();
-            int score = scan.nextInt();
-            if (subStr.length() >= score) continue;//길이당 1점인데 점수가 그것보다 낮으면 최대점수가 될 수 없음.
+    static List<pair> ava[];
+    static int DP[];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            int index = 0; //각 인덱스의 위치에서 삭제가 가능한지 확인
-            while (index < S.length()) {
-                int pos = S.indexOf(subStr, index); //가장 가까운 매칭 위치 찾기
-                if (pos == -1) break;
-                // 문자열이 매칭되었다면 그 위치에 (문자열 길이, 점수) 기록
-                strings.get(pos).add(new Pair(subStr.length(), score));
-                index = pos + 1;
+        String S = br.readLine();
+        int len = S.length();
+        DP = new int[len + 1];
+        ava = new ArrayList[len];
+        for(int i = 0; i < len; ++i)ava[i] = new ArrayList<>();
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; ++i){
+            String []str = br.readLine().split(" ");
+            int score = Integer.parseInt(str[1]);
+            int strLen = str[0].length();
+            if(strLen >= score) continue;
+
+            int idx = 0;
+            while(idx < len) {
+                int cur = S.indexOf(str[0], idx);
+                if(cur == -1) break;
+                ava[cur].add(new pair(strLen, score));
+                idx = cur + 1;
             }
         }
-        
-        // 다이나믹 프로그래밍 수행
-        for (int i = 0; i < S.length(); i++) { //각 인덱스마다
-            dp[i+1] = Math.max(dp[i+1], dp[i]+1);   //한문자 지우는경우와 비교
-            for (int j = 0; j < strings.get(i).size(); j++) {// 지울 수 있는 부분 문자열들 확인
-                int length = strings.get(i).get(j).first; // 부분 문자열의 길ㅇ
-                int score = strings.get(i).get(j).second; // 점수
-                dp[i + length] = Math.max(dp[i + length], dp[i] + score);
+
+        for(int i = 0; i < len; ++i){
+            for(int j = 0; j < ava[i].size(); ++j){
+                int siz = ava[i].get(j).siz;
+                int score = ava[i].get(j).score;
+                DP[i + siz] = Math.max(DP[i + siz], DP[i] + score);
             }
-        }
-        System.out.println(dp[S.length()]);
-    }   
-    
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        FastReader (){
-            br = new BufferedReader(new InputStreamReader(System.in));
+            DP[i + 1] = Math.max(DP[i + 1], DP[i] + 1);
         }
 
-        String next() {
-            while (st == null || !st.hasMoreTokens()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                }catch (IOException e){
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-        
+        System.out.println(DP[len]);
     }
 }
