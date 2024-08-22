c6746644b7f6836c6b6b6e3a4d39b987c887ee1c
subo <82173071+subo-9439@users.noreply.github.com>
2024-08-22 09:47:34 +0900

    [Bronze II] Title: 전구, Time: 584 ms, Memory: 42404 KB -BaekjoonHub

--- 백준/Bronze/21918. 전구/전구.java
+++ 백준/Bronze/21918. 전구/전구.java
@@ -1,33 +1,23 @@
 import java.util.*;
 
 public class Main {
-    public static int N,M;
+    public static int N, M;
     public static int[] arr = new int[4001];
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-        for (int i = 1; i <= N; i++) {
-            arr[i] = sc.nextInt();
+        
+        for (int i = 1; i <= N; i++) arr[i] = sc.nextInt();
+        
+        while (M-- > 0) {
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+            if (a == 1) arr[b] = c;
+            //2일경우에는 xor 이용하여 변경, 3일경우:전부끄기(0), 4일경우:전부켜기(1)
+            else for (int j = b; j <= c; j++) arr[j] = (a == 2) ? arr[j] ^ 1 : (a == 3) ? 0 : 1;
         }
         
-        for (int i = 0; i < M; i++) {
-            int a = sc.nextInt(); // 명령어
-            int b = sc.nextInt(); // 전구번호
-            int c = sc.nextInt(); // 키거나 끄기 or 전구번호
-            if(a == 1) {//b번에 해당하는 전구 c하기
-              arr[b] = c; 
-            }else{
-              for(int j = b; j <= c; j++) {
-                arr[j] = a == 2 ? arr[j] ^ 1 : (a == 3 ? 0 : 1);
-              }
-            }
-        }
-        
-        for(int i = 1 ; i <= N; i++) {
-            System.out.print(arr[i]+" ");
-        }
+        for (int i = 1; i <= N; i++) System.out.print(arr[i] + " ");
     }
-    
 }
