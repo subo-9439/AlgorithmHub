6cb7dc32062115e9752fe01908ce56914aaef7e4
subo <82173071+subo-9439@users.noreply.github.com>
2025-01-25 13:00:19 +0900

    [Bronze I] Title: 단어 공부, Time: 268 ms, Memory: 21104 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,33 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        FastReader scan = new FastReader();
+        String str = scan.next().toUpperCase();
+        int[] words = new int[26];
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        str = str.toUpperCase();
-
-        int[] arr = new int[26];
-        int max = 0;
-        int index = 0;
-        for(int i=0;i<str.length();i++){
-            int alpha = str.charAt(i);
-            arr[alpha-65]++;
+        // 1) 빈도수 계산
+        for (int i = 0; i < str.length(); i++) {
+            words[str.charAt(i) - 'A']++;
         }
-        for(int i=0; i<arr.length;i++){
-            if(max <arr[i]){
-                max = arr[i];
-                index = i+65;
-            }
-            else if(max == arr[i]){
-                index = '?';
-            }
 
+        // 2) 최대빈도 찾기
+        int maxCount = -1;
+        char answer = '?';
+        for (int i = 0; i < 26; i++) {
+            if (words[i] > maxCount) {
+                maxCount = words[i];
+                answer = (char) ('A' + i);
+            }
         }
-        System.out.println((char)index)
-        ;
+
+        // 3) 최대빈도를 갖는 알파벳이 여러 개인지 확인
+        int countMaxAlphabet = 0;
+        for (int i = 0; i < 26; i++) {
+            if (words[i] == maxCount) {
+                countMaxAlphabet++;
+                if (countMaxAlphabet > 1) {
+                    answer = '?';
+                    break;
+                }
+            }
+        }
+        
+        System.out.println(answer);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        public String next(){
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st= new StringTokenizer(br.readLine());
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
     }
 }
