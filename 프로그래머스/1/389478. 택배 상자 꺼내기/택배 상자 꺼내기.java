f6ffdaf091bc49d354c0c21b90547a2e1e440da0
subo <82173071+subo-9439@users.noreply.github.com>
2025-03-09 18:46:08 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.06 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,37 +1,25 @@
 class Solution {
     public int solution(int n, int w, int num) {
-        int answer = 0;
-        int[][] map = new int[(n + w - 1)/w][w];
-        
-        int val = 0;
-        int xIdx = 0, yIdx = 0;
-        boolean flag = false;
+        int rows = (n + w - 1) / w;
+        int[][] map = new int[rows][w];
 
-        for (int i = 0; i < map.length; i++){
-            for(int j = 0; j < w; j++) {
+        int val = 0, xIdx = 0, yIdx = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < w; j++) {
                 val++;
-                
-                if(i % 2 == 0){
-                    map[i][j] = val;                    
-                }else {
-                    map[i][w-j-1] = val; 
-                }
-                if(val == num){
+
+                int col = (i % 2 == 0) ? j : (w - j - 1);
+                map[i][col] = val;
+
+                if (val == num) {
                     xIdx = i;
-                    yIdx = (i % 2 == 0) ? j : (w - j - 1);
-                    
+                    yIdx = col;
                 }
-                if(val == n) {
-                    flag = true; 
-                    break;
-                }
+                if (val == n) break;
             }
-            if(flag){
-                break;
-            }
+            if (val == n) break;
         }
-        if(map[map.length-1][yIdx] == 0) answer = map.length - xIdx-1;
-        else answer = map.length - xIdx;
-        return answer;
+        return (map[rows - 1][yIdx] == 0) ? rows - xIdx - 1 : rows - xIdx;
     }
 }
