f6f5844e5ceb5307c0f3be8443d78ea170d6e3b1
subo <82173071+subo-9439@users.noreply.github.com>
2025-05-16 21:52:31 +0900

    [level 2] Title: 롤케이크 자르기, Time: 197.00 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.ArrayList;    
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -6,27 +6,37 @@ import java.util.LinkedList;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        HashSet<Integer> set1 = new HashSet<>();
-        HashMap<Integer,Integer> set2 = new HashMap<>();
-        // 각 토핑의 수를 세기
-        for (int i = 0; i < topping.length; i++) {
-            set2.put(topping[i],set2.getOrDefault(topping[i],0)+1);
-        }
         
-        //오른쪽 조각 토핑개수세기
-        int cnt = set2.size();
-        for (int i = 0; i < topping.length; i++) {
-            set1.add(topping[i]);
-            if (set2.get(topping[i]) !=0 ){
-                if (set2.get(topping[i])-1 == 0) cnt--;
-                
-                set2.put(topping[i], set2.get(topping[i]) - 1);
+        // 왼쪽 종류 카운트를 위한 Set
+        HashSet<Integer> leftSet = new HashSet<>();
+        
+        // 오른쪽 종류 카운트를 위한 Map
+        HashMap<Integer, Integer> rightMap = new HashMap<>();
+        for (int t : topping) {
+            rightMap.put(t, rightMap.getOrDefault(t, 0) + 1);
+        }
+
+        // 오른쪽 조각의 종류 수
+        int rightCount = rightMap.size();
+        
+        // 순회하면서 왼쪽과 오른쪽 비교
+        for (int t : topping) {
+            leftSet.add(t);
+            
+            // 오른쪽 Map에서 개수 줄이고 0이 되면 제거
+            int currentCount = rightMap.get(t);
+            if (currentCount == 1) {
+                rightMap.remove(t);
+            } else {
+                rightMap.put(t, currentCount - 1);
             }
             
-            if (set1.size() == cnt){
+            // 사이즈(종류 수)가 같으면 경우의 수 추가
+            if (leftSet.size() == rightMap.size()) {
                 answer++;
             }
         }
+
         return answer;
     }
 }
